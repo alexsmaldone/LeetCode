8df5c682c4fae033ed7cec72acd6b1916c50ff49
alexsmaldone <alexsmaldone@gmail.com>
2022-05-18 21:58:20 -0400

    lowest common acnestor

---
+++ trees/lowest_common_Ancestor_bst.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        p_val = p.val
+        q_val = q.val
+
+        node = root
+
+        while node:
+            parent_val = node.val
+
+            if p_val > parent_val and q_val > parent_val:
+                node = node.right
+            elif p_val < parent_val and q_val < parent_val:
+                node = node.left
+            else:
+                return node
