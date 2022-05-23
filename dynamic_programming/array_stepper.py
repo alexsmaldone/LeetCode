323e0d04f47c44dd0ebd79d93a65d8413c13a836
alexsmaldone <alexsmaldone@gmail.com>
2022-05-22 21:19:07 -0400

    array stepper

--- dynamic_programming/array_stepper.py
+++ dynamic_programming/array_stepper.py
@@ -1,2 +1,20 @@
 # return boolean indicating whether possible to reach last position of the list
 # each number in list indicates number of steps (maximum) you may take
+def array_stepper(numbers):
+  return _array_stepper(numbers, 0, {})
+
+def _array_stepper(numbers, i, memo):
+  if i in memo:
+    return memo[i]
+
+  if i >= len(numbers) - 1:
+    return True
+
+  max_step = numbers[i]
+  for step in range(1, max_step + 1):
+    if _array_stepper(numbers, i + step, memo):
+      memo[i] = True
+      return True
+
+  memo[i] = False
+  return False
