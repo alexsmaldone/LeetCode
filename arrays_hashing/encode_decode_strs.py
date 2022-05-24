396507c25457ce2a2578cdbd546cf085df1e9a1b
alexsmaldone <alexsmaldone@gmail.com>
2022-05-23 20:51:32 -0400

    encode and decode strings

---
+++ arrays_hashing/encode_decode_strs.py
@@ -0,0 +1,23 @@
+class Codec:
+
+    def encode(self, strs):
+        """Encodes a list of strings to a single string.
+
+        :type strs: List[str]
+        :rtype: str
+        """
+        return unichr(257).join(strs)
+
+    def decode(self, s):
+        """Decodes a single string to a list of strings.
+
+        :type s: str
+        :rtype: List[str]
+        """
+        return s.split(unichr(257))
+
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))
