c580b59da82531ce1b98396f8ba09d2b8c8b6280
alexsmaldone <alexsmaldone@gmail.com>
2022-05-26 19:55:08 -0400

    reverse polish notation

--- stack/reverse_polish_notation.py
+++ stack/reverse_polish_notation.py
@@ -1,3 +1,26 @@
 # Evaluate the value of an arithmetic expression in Reverse Polish Notation.
 
 # Valid operators are +, -, *, and /. Each operand may be an integer or another expression.
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        operations = {
+        "+": lambda a, b: a + b,
+        "-": lambda a, b: a - b,
+        "/": lambda a, b: int(a / b),
+        "*": lambda a, b: a * b
+        }
+
+        stack = []
+        for token in tokens:
+            if token in operations:
+                number_2 = stack.pop()
+                number_1 = stack.pop()
+                operation = operations[token]
+                stack.append(operation(number_1, number_2))
+            else:
+                stack.append(int(token))
+        return stack.pop()
