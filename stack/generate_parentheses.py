22fda9e42d513a998f71107ee17daa98ec71fd0f
alexsmaldone <alexsmaldone@gmail.com>
2022-05-26 19:56:21 -0400

    generate wellformed parentheses

--- stack/generate_parentheses.py
+++ stack/generate_parentheses.py
@@ -1,1 +1,26 @@
 # Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        stack = []
+        result = []
+
+        def backtrack(openN, closedN):
+            if openN == closedN == n:
+                result.append("".join(stack))
+                return
+
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop()
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+
+        backtrack(0,0)
+        return result
