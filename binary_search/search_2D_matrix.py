af2c4d8ab6ba55c4fcd9283f245214948b1903fa
alexsmaldone <alexsmaldone@gmail.com>
2022-05-26 20:00:49 -0400

    serach a 2d matrix

--- binary_search/search_2D_matrix.py
+++ binary_search/search_2D_matrix.py
@@ -2,3 +2,29 @@
 
 # Integers in each row are sorted from left to right.
 # The first integer of each row is greater than the last integer of the previous row.
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        row = len(matrix)
+        if row == 0:
+            return False
+        col = len(matrix[0])
+
+        #binary search
+        left, right = 0, row * col - 1
+        while left <= right:
+            pivot_idx = (left + right) // 2
+            print(left, right, pivot_idx)
+            pivot_element = matrix[pivot_idx // col][pivot_idx % col]
+            if target == pivot_element:
+                return True
+            else:
+                if target < pivot_element:
+                    right = pivot_idx - 1
+                else:
+                    left = pivot_idx + 1
+        return False
