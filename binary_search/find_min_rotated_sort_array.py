6267e2f32e86840e2495fd01ef89bedaea9b1c1e
alexsmaldone <alexsmaldone@gmail.com>
2022-05-26 20:03:59 -0400

    find min rotated sorted array

--- binary_search/find_min_rotated_sort_array.py
+++ binary_search/find_min_rotated_sort_array.py
@@ -1,3 +1,31 @@
 # Given the sorted rotated array nums of unique elements, return the minimum element of this array.
 
 # You must write an algorithm that runs in O(log n) time.
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # If the list has just one element then return that element.
+        if len(nums) == 1:
+            return nums[0]
+
+        # left pointer
+        left = 0
+        # right pointer
+        right = len(nums) - 1
+
+        if nums[right] > nums[0]:
+            return nums[0]
+
+        while left <= right:
+            mid = left + (right - left) / 2
+            if nums[mid] > nums[mid + 1]:
+                return nums[mid + 1]
+            if nums[mid - 1] > nums[mid]:
+                return nums[mid]
+            if nums[mid] > nums[0]:
+                left = mid + 1
+            else:
+                right = mid - 1
