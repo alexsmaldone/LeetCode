1a53db14b4198d1700eb36e71e7a20cffc719a55
alexsmaldone <alexsmaldone@gmail.com>
2022-05-29 19:08:12 -0400

    add good nodes

--- trees/count_Good_nodes.py
+++ trees/count_Good_nodes.py
@@ -1,3 +1,23 @@
 # Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
 
 # Return the number of good nodes in the binary tree.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+
+        def dfs(node, maxVal):
+            if not node:
+                return 0
+
+            result = 1 if node.val >= maxVal else 0
+            maxVal = max(node.val, maxVal)
+
+            result += dfs(node.left, maxVal)
+            result += dfs(node.right, maxVal)
+
+            return result
+        return dfs(root, root.val)
