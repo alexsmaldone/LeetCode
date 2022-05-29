d99080be0910b3f0599f67c0a92c2d374f35cb62
alexsmaldone <alexsmaldone@gmail.com>
2022-05-29 00:47:44 -0400

    find udplicate number

--- linked_lists/find_duplicate_num.py
+++ linked_lists/find_duplicate_num.py
@@ -3,3 +3,22 @@
 # There is only one repeated number in nums, return this repeated number.
 
 # You must solve the problem without modifying the array nums and uses only constant extra space.
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        slow, fast = 0, 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
