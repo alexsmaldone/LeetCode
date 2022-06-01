50cba36949f9b0c4c8bbf07a07a33f645552f8ea
alexsmaldone <alexsmaldone@gmail.com>
2022-05-31 22:17:14 -0400

    implement trie

--- tries/implement_Trie.py
+++ tries/implement_Trie.py
@@ -4,3 +4,46 @@
 # void insert(String word) Inserts the string word into the trie.
 # boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
 # boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into a data structure
+        """
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+
+        curr.isEnd = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+
+        return curr.isEnd
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+
+        for c in prefix:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+
+        return True
