023540c1ce004c41add82823c5965dcb27150de6
alexsmaldone <alexsmaldone@gmail.com>
2022-05-31 22:18:09 -0400

    search word data structure

--- tries/search_word_ds.py
+++ tries/search_word_ds.py
@@ -3,3 +3,41 @@
 # WordDictionary() Initializes the object.
 # void addWord(word) Adds word to the data structure, it can be matched later.
 # bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise. word may contain dots '.' where dots can be matched with any letter.
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+
+        curr.isEnd = True
+
+
+    def search(self, word: str) -> bool:
+        def dfs(j, root):
+            cur = root
+
+            for i in range(j, len(word)):
+                c = word[i]
+                if c == ".":
+                    for child in cur.children.values():
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+                else:
+                    if c not in cur.children:
+                        return False
+                    cur = cur.children[c]
+            return cur.isEnd
+
+        return dfs(0, self.root)
