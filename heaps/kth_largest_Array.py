e47d7a4b28f7a4472df7af1413ce30dd3e07462f
alexsmaldone <alexsmaldone@gmail.com>
2022-05-31 22:20:53 -0400

    kth largest element]

--- heaps/kth_largest_Array.py
+++ heaps/kth_largest_Array.py
@@ -1,3 +1,25 @@
 # Given an integer array nums and an integer k, return the kth largest element in the array.
 
 # Note that it is the kth largest element in the sorted order, not the kth distinct element.
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+
+        # return heapq.nlargest(k, nums)[-1]
+
+        k = len(nums) - k
+
+        def quickSelect(l, r):
+            if l == r: return nums[l]
+
+            pivot, p = nums[r], l
+            for i in range(l, r):
+                if nums[i] <= pivot:
+                    nums[p], nums[i] = nums[i], nums[p]
+                    p += 1
+            nums[p], nums[r] = nums[r], nums[p]
+
+            if p > k: return quickSelect(l, p - 1)
+            elif p < k: return quickSelect(p + 1, r)
+            else: return nums[p]
+
+        return quickSelect(0, len(nums) - 1)
